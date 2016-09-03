3ebd0d279a390620b54cba7af6208f952ccc50e2
Callan Morrison <callan.l.morrison@gmail.com>
2016-09-02 22:37:46 -0500

    core project functionality in place

--- app/controllers/project.ctrl.js
+++ app/controllers/project.ctrl.js
@@ -1,8 +1,9 @@
-app.controller('projectCtrl', function ($scope, $routeParams, projectFactory) {
+app.controller('projectCtrl', function ($scope, $sce, $routeParams, projectFactory) {
   $scope.projectToDisplay = "";
 
   projectFactory.getSpecificProjectDetails($routeParams.projectName).then((project) => {
     $scope.projectToDisplay = project;
+    $scope.projectToDisplay.url = $sce.trustAsResourceUrl($scope.projectToDisplay.url)
   })
 
 })
